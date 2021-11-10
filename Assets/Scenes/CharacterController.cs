75ff1da303595d3a1b13024311c1855492bb32a0
TinHuy <thuynh2021@elam.co.uk>
2021-11-10 12:33:29 +0000

    I add a jump code

--- Assets/Scenes/CharacterController.cs
+++ Assets/Scenes/CharacterController.cs
@@ -10,18 +10,31 @@ public class CharacterController : MonoBehaviour
     GameObject cam;
     public float rotationSpeed = 2.0f;
     public float camRotationSpeed = 1.5f;
+    Rigidbody myRigidbody;
+
+    bool isOnGround;
+    public GameObject groundChecker;
+    public LayerMask groundLayer; 
+    
+
 
     // Start is called before the first frame update
     void Start()
     {
         cam = GameObject.Find("Main Camera");
+        myRigidbody = GetComponent<Rigidbody>(); 
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = transform.position + (transform.forward * Input.GetAxis("Vertical"));
+
+        isOnGround = Physics.CheckSphere(groundChecker.transform.position, 0.1f, groundLayer);
+
         transform.position = transform.position + (transform.right * Input.GetAxis("Horizontal"));
+
+        Vector3 newVelocity = transform.forward * Input.GetAxis("Vertical") * maxSpeed;
+        myRigidbody.velocity = new Vector3(newVelocity.x, myRigidbody.y, newVelocity.z);
 
         rotation = rotation + Input.GetAxis("Mouse X") * rotationSpeed;
         transform.rotation = Quaternion.Euler(new Vector3(0.0f, rotation, 0.0f));
