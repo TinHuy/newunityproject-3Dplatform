c10f055c9a4fe030bfbb78e664f2de0921127631
TinHuy <thuynh2021@elam.co.uk>
2021-11-12 11:49:47 +0000

    jump and sprint

--- Assets/Scenes/CharacterController.cs
+++ Assets/Scenes/CharacterController.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CharacterController : MonoBehaviour
-{       
-    public float maxSpeed = 1.0f;
+{
+    public float maxSpeed;
     float rotation = 0.0f;
     float camRotation = 0.0f;
     GameObject cam;
@@ -13,14 +13,23 @@ public class CharacterController : MonoBehaviour
     Rigidbody myRigidbody;
 
     bool isOnGround;
-    public GameObject groundChecker;
-    public LayerMask groundLayer; 
-    
+    public GameObject GroundChecker;
+    public LayerMask GroundLayer;
+    public float jumpForce = 300.0f;
 
+    public float normalSpeed = 10.0f;
+    public float SprintSpeed = 20.0f;
+
+    public float masSprint = 5.0f;
+    float sprintTimer;
 
     // Start is called before the first frame update
     void Start()
     {
+        Cursor.lockState = CursorLockMode.Locked;
+
+        sprintTimer = maxSpeed;
+
         cam = GameObject.Find("Main Camera");
         myRigidbody = GetComponent<Rigidbody>(); 
     }
@@ -29,12 +38,32 @@ public class CharacterController : MonoBehaviour
     void Update()
     {
 
-        isOnGround = Physics.CheckSphere(groundChecker.transform.position, 0.1f, groundLayer);
+        isOnGround = Physics.CheckSphere(GroundChecker.transform.position, 0.1f, GroundLayer);
 
+        if (isOnGround == true && Input.GetKeyDown(KeyCode.Space))
+        {
+            myRigidbody.AddForce(transform.up * jumpForce);
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift) && sprintTimer > 0.0f)
+        {
+            maxSpeed = SprintSpeed;
+            sprintTimer = sprintTimer - Time.deltaTime;
+        } else
+        {
+            maxSpeed = normalSpeed;
+            if (Input.GetKey(KeyCode.LeftShift) == false)  {
+                sprintTimer = sprintTimer + Time.deltaTime;
+            }
+        }
+
+        sprintTimer = Mathf.Clamp(sprintTimer, 0.0f, masSprint);
+
+        transform.position = transform.position + (transform.forward * Input.GetAxis("Vertical"));
         transform.position = transform.position + (transform.right * Input.GetAxis("Horizontal"));
 
-        Vector3 newVelocity = transform.forward * Input.GetAxis("Vertical") * maxSpeed;
-        myRigidbody.velocity = new Vector3(newVelocity.x, myRigidbody.y, newVelocity.z);
+        Vector3 newVelocity = transform.forward * Input.GetAxis("Vertical") * maxSpeed + (transform.right * Input.GetAxis("Horizontal") * maxSpeed);
+        myRigidbody.velocity = new Vector3(newVelocity.x, myRigidbody.velocity.y, newVelocity.z);
 
         rotation = rotation + Input.GetAxis("Mouse X") * rotationSpeed;
         transform.rotation = Quaternion.Euler(new Vector3(0.0f, rotation, 0.0f));
